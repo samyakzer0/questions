8c088e42910732d71df2090592838304e5ede1d1
SMYK <samyak.sage@gmail.com>
2026-02-08 13:07:43 +0530

    Add Bridge.cpp

---
+++ Bridge.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+
+    for (int i = n ; i >= 1 ; i--){
+        for (int j = 1 ; j <= i ; j++){
+            cout << char(64+j) << " ";
+        }
+
+        for (int j = 1 ; j <= 2*(n-i)-1 ; j++){
+            cout << "  ";
+        }
+        if(i!=n){
+        for (int j = i ; j >= 1 ; j--){
+            cout << char(64+j) << " ";
+        }
+    }    else {
+            for (int j = i  ; j >= 1; j--){
+                cout << char(64+j) << " ";
+            }
+        }
+        cout << endl;
+}
+}
